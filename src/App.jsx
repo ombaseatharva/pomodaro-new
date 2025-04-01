45a177dbadabf846bcf61ffaec6cd75fd5e9da4b
ombaseatharva <ombaseatharva04@gmail.com>
2025-04-01 16:15:37 +0530

    new2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import PomodoroTimer from "./PomodoroTimer";
+import PomodoroTimer from "./pomodorotimer";
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
+      <div>
       <h1>Pomodoro Tracker</h1>
       <PomodoroTimer />
+      </div>
     </div>
   );
 }
